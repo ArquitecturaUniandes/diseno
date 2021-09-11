71112a44257c04b810b385d0cd4fa764b8a5b6e2
Mateo Castaño Gomez <mateocastanogomez@Mateos-MacBook-Pro.local>
2021-09-11 18:28:39 -0500

    Missing class name

--- history/api_queries.py
+++ history/api_queries.py
@@ -3,11 +3,11 @@ from base import app, api, History, history_schema,  q, Resource, Flask, request
 
 class ClinicalHistoryResource(Resource):
     def get(self, numero_historia):
-        obj = ClinicalHistory.query.get_or_404(numero_historia)
+        obj = History.query.get_or_404(numero_historia)
         return history_schema.dump(obj)
 
 
-api.add_resource(ClinicalHistoryResource, '/api-queries/clinical-history/<int:numero_historia>')
+api.add_resource(ClinicalHistoryResource, '/api-queries/history/<int:numero_historia>')
 
 
 if __name__ == '__main__':
